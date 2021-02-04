ccea2c84c26c8b6a002456bf43db451f127a0c02
annemariedupont <annemarie.abdelaziz@gmail.com>
2021-02-04 10:20:46 +0100

    Màj

--- main.js
+++ main.js
@@ -1,23 +1,51 @@
 window.onload = function(){
-    var bodyHtml = document.querySelector('body');
-    var jeuCarte = [];
-for(var i=0; i<52; i++){
+    let bodyHtml = document.querySelector('body');
+
+let typeCarte = ["pique","coeur","carreau","trefle"];
+let Nomcarte = ["As","Roi","Dame","Valet",10,9,8,7,6,5,4,3,2];
+let JeuCartes=[];
+for(let i =0; i<4;i++){
+    for(let j = 0;j<4;){
+        let carte = {typeCarte,Nomcarte};
+        JeuCartes.push(carte);
+    }
+}
+console.log(JeuCartes);
+
+
+    let jeuCarte = [];
+for(let k=0; k<52; k++){
     
-    var imageHtml = document.createElement('img');
+    let imageHtml = document.createElement('img');
     bodyHtml.appendChild(imageHtml);
-    imageHtml.src="https://via.placeholder.com/150";
+    imageHtml.src = "https://via.placeholder.com/150?text=Mystery+Card";
+    //imageHtml.id = k;
+    imageHtml.addEventListener('click',FaceCarte);
+    imageHtml.addEventListener('dblclick', FaceCache);
     jeuCarte.push(imageHtml);
     console.log(imageHtml.src);
-    imageHtml.src = imageHtml.src + "?text=Mystery+Card";
-
+    
 }
 console.log(jeuCarte);
 console.log(imageHtml.src);
-var urlImage = "https://via.placeholder.com/150";
-var UrlRoi = {nom: "?text=Roi",couleur : "FF0000/000000"};
-var UrlDame = {nom: "?text=Dame",couleur : "FF0000/000000"};
-var UrlValet = {nom: "?text=Valet",couleur : "FF0000/000000"};
-var UrlAs = {nom: "?text=As",couleur : "FF0000/000000"};
+
+
+function FaceCarte(event){
+    console.log(event.id);
+        let urlImage = 'https://via.placeholder.com/150/';
+        let color = "FF0000/000000";
+        let text = "?text=";
+        event.src = urlImage + color + text + JeuCartes.Nomcarte;
+
+}
+
+function FaceCache(event) {
+    console.log('dblclick on ' + event.id);
+    event.src = 'https://via.placeholder.com/150?text=Mistery+Card';
+
+
+
+}
 
 
 
